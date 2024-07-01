67b69787d6ee1a67caf0532b6746cb246c413451
Premium07 <premsagarg23@gmail.com>
2024-07-01 10:57:08 +0545

    insertDateandTimeinproject

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,10 +1,11 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 // import { FaCheck } from "react-icons/fa";
 import { MdDeleteForever } from "react-icons/md";
 
 const Todo = () => {
   const [data, setData] = useState("");
   const [task, setTask] = useState([]);
+  const [dateTime, setDateTime] = useState(new Date());
 
   const handleInputChange = (value) => {
     setData(value);
@@ -26,33 +27,46 @@ const Todo = () => {
     setTask((prev) => [...prev, data]);
     setData("");
   };
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setDateTime(new Date());
+    }, 1000);
+    return () => clearInterval(interval);
+  });
   return (
     <section className="max-w-xl mx-auto p-2 ">
       <header className="mt-5">
         <h1 className="text-2xl text-center font-semibold">Todo List</h1>
+        <h2 className="text-center mt-2 text-xl">
+          {dateTime.toLocaleDateString()} - {dateTime.toLocaleTimeString()}
+        </h2>
       </header>
-      <section className="mt-5">
+      <section className="mt-5 w-full">
         <form
           className="flex justify-between  border-black border rounded-sm"
           onSubmit={handleSubmit}
         >
-          <div className="">
+          <div className="w-3/4">
             <input
               type="text"
-              className="py-2 outline-none px-3 w-full"
+              className="py-2 outline-none px-3  w-full"
               autoComplete="off"
               value={data}
               onChange={(e) => handleInputChange(e.target.value)}
             />
           </div>
-          <div className="">
-            <button type="submit" className="px-5 py-2 bg-black text-white">
+          <div className="w-1/4">
+            <button
+              type="submit"
+              className="px-5 py-2 bg-black text-white w-full"
+            >
               Add Task
             </button>
           </div>
         </form>
       </section>
-      <section className="w-full">
+      <section className="w-full mt-2">
         <ul className="w-full">
           {task.map((item, index) => {
             return (
@@ -60,7 +74,7 @@ const Todo = () => {
                 key={index}
                 className="w-full flex justify-between items-center rounded-sm mt-5 text-xl border-b text-white bg-black p-2 px-4"
               >
-                <span className="">{item.toUpperCase()}</span>
+                <span className="">{item}</span>
                 {/* <button className="p-2 bg-black text-white rounded-full">
                   <FaCheck />
                 </button> */}
